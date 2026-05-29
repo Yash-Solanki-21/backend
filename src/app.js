b8752fdd8694111e8654f96b26a4f2abb48efda5
Yash Solanki <yashsolankiji21@gmail.com>
2026-05-29 18:08:42 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit="16kb"}))
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
+
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users", userRouter)
 
 export {app}
